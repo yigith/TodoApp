4def657b08cdf68f9b3d2abf9fa735200fe1bb28
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-03-05 13:10:43 +0300

    add new todo

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -3,6 +3,23 @@ import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([{ name: "Loading.."}]);
+  const [newTodo, setNewTodo] = useState({ id: 0, name: "", isCompleted: false});
+
+  const addNewTodo = (e) => {
+    e.preventDefault();
+    fetch("https://localhost:5001/api/TodoItems", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newTodo)
+    })
+    .then(response => response.json())
+    .then(json => {
+      setTodos([...todos, json]);
+      setNewTodo({ id: 0, name: "", isCompleted: false});
+    });
+  } 
 
   useEffect(() => {
     // https://jsonplaceholder.typicode.com/todos
@@ -14,6 +31,12 @@ function App() {
   return (
     <div className="App">
       <h1>My Todos</h1>
+      <form onSubmit={addNewTodo}>
+        <input type="text" 
+          value={newTodo.name} 
+          onInput={e=>setNewTodo({...newTodo, name: e.target.value})} 
+          placeholder="what will you do?" required />
+      </form>
       <ul>
         {todos.map((x, i) => <li key={i}>{x.name}</li>)}
       </ul>
